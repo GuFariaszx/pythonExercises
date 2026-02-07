3ee769ff6f9d2b043fdff5acf0b835c64e2c4375
= <=>
2026-02-06 22:16:04 -0300

    exercise media and perimetro (Refinado) add

--- retanguloArea.py
+++ retanguloArea.py
@@ -1,9 +1,10 @@
 ladoRetangulo = float(input('Digite o lado do Retângulo:'))
 baseRetangulo = float(input('Digite a base do Retângulo:'))
+unidadeMedida = str(input('Digite a Unidade de medida dos valores: (pl)'))
 
 somaDosLados = ladoRetangulo + ladoRetangulo + baseRetangulo + baseRetangulo
 resultPerimetro = somaDosLados
 resultArea = ladoRetangulo * baseRetangulo
 
-print(f'O perímetro de seu Retângulo é {resultPerimetro:.2f}')
-print(f'A Área de seu Retângulo é {resultArea:.2f}')
+print(f'O perímetro de seu Retângulo é {resultPerimetro:.2f} {unidadeMedida}')
+print(f'A Área de seu Retângulo é {resultArea:.2f} {unidadeMedida} cúbico')
